b4fd1fa4f118ffa9dac961b6f1b7c2506d063f7e
acepilot0620 <>
2021-02-27 00:57:33 +0900

    postgresql 설정 제거

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -10,9 +10,3 @@ MIDDLEWARE += [
 
 INTERNAL_IPS = ['127.0.0.1']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -1,12 +1,1 @@
 from .common import *
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'backend_carryme',
-        'USER': 'postgres',
-        'PASSWORD': 'chlwjdgh97!',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
